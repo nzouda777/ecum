6c47c70ce6e0ce2eeea9799b944f40b00af59f99
nzouda777 <nzoudarodrigue@gmail.com>
2024-05-12 19:20:09 +0200

    maj

--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -109,7 +109,7 @@ const showClause = ref(false)
                 >
             </div>
             <div class=" space-y-12">
-                <h1 class="text-center uppercase text-5xl text-indigo-900 w-12/12 mx-auto font-bold font-serif">
+                <h1 class="text-center uppercase md:text-5xl text-2xl text-indigo-900 w-12/12 mx-auto font-bold font-serif">
                     CHOISIR LE MEILLEUR COLLAGÈNE MARIN : QUEL TYPE, QUELLE FORME ET POURQUOI?
                 </h1>
                 
@@ -135,9 +135,7 @@ const showClause = ref(false)
         </div>
     </section>
     <section class="max-w-[800px] py-2 px-4 mx-auto mt-8 space-y-5 ">
-        <h2 class="text-center md:text-5xl text-3xl  font-bold">
-            nos produits.
-        </h2>
+        
         <p class="uppercase tracking-2 leading-2 md:w-2/3 w-full text-center mx-auto text-[#444]">
             ÉCUME VOUS PROPOSE UNE GAMME DE 2 GOÛTS DIFFÉRENTS, AFIN DE SUBLIMER VOTRE QUOTIDIEN
         </p>
@@ -147,86 +145,19 @@ const showClause = ref(false)
                 <div class="flex justify-between items-end">
                     <div class="mt-2 space-y-0.5">
                         <h3 class="font-bold text-xl">arôme fraise.</h3>
-                        <div class="flex gap-1 items-center">
-                            <div class="stars flex">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-
-                            </div>
-                            <p class="text-sm">28 Avis</p>
-                        </div>
-                        <p class="text-sm font-light text-gray-600">
-                            BOÎTE MENSUELLE
-                        </p>
-                        <p class="text-xs font-light text-gray-600">
-                            28 STICKS
-                        </p>
-                    </div>
-                    <div class="">
-                        <p class="text-xs font-light text-gray-600">
-                            À PARTIR DE
-                        </p>
-                        <p class="font-bold tracking-4 text-base">
-                            49,90 €
-                        </p>
                     </div>
                 </div>
+                <button class="border w-full border-black text-base mt-2 py-2  text-center hover:text-white hover:bg-black hover:duration-500">Decouvrir sur le site</button>
             </a>
             <a href="https://ecumecollagen.com/collections/frontpage/products/citron" class="w-full h-fit mx-auto">
                 <img class="md:w-full w-full object-cover h-2/3" src="https://ecumecollagen.com/cdn/shop/products/Boxlemonopen.png?v=1678474511" alt="">
                 <div class="flex justify-between items-end">
                     <div class="mt-2 space-y-0.5">
                         <h3 class="font-bold text-xl">arôme citron.</h3>
-                        <div class="flex gap-1 items-center">
-                            <div class="stars flex">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" class="w-4 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                </svg>
-
-                            </div>
-                            <p class="text-sm">24 Avis</p>
-                        </div>
-                        <p class="text-sm font-light text-gray-600">
-                            BOÎTE MENSUELLE
-                        </p>
-                        <p class="text-xs font-light text-gray-600">
-                            28 STICKS
-                        </p>
-                    </div>
-                    <div class="">
-                        <p class="text-xs font-light text-gray-600">
-                            À PARTIR DE
-                        </p>
-                        <p class="font-bold tracking-4 text-base">
-                            49,90 €
-                        </p>
+                        
                     </div>
                 </div>
+                <button class="border w-full border-black text-base mt-2 py-2  text-center hover:text-white hover:bg-black hover:duration-500">Decouvrir sur le site</button>
             </a>
         </div>
     </section>
